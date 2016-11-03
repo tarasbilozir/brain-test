1aac4edca6e01ceec204bc861ce2f4a1913e3d05
tatomyr <andrew.tatomyr@gmail.com>
2016-11-03 15:46:53 +0200

    Intermediate working

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,3 +1,5 @@
+const d = 100;
+
 import React from 'react';
 
 import brain from 'brain';
@@ -8,68 +10,50 @@ import brain from 'brain';
 export default class App extends React.Component {
 
   paint(event) {
+    event.persist();
+    // const canvas = document.getElementById('canvas');
+     ctx = this.refs.canvas.getContext('2d');
+
+    global.e = event;
 
 
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
 
-
-
-
-
-    var x = event.layerX;
-    var y = event.layerY;
+    var x = event.nativeEvent.offsetX;
+    var y = event.nativeEvent.offsetY;
     console.log(event,x,y)
 
-    // var pixel = ctx.getImageData(x, y, 1, 1);
-    // var data = pixel.data;
-    // var rgba = 'rgba(' + data[0] + ',' + data[1] +
-    //            ',' + data[2] + ',' + (data[3] / 255) + ')';
+     pixel = ctx.getImageData(x, y, 1, 1);
+     data = pixel.data;
+    var rgba = 'rgba(' + data[0] + ',' + data[1] +
+               ',' + data[2] + ',' + (data[3] / 255) + ')';
     // color.style.background =  rgba;
     // color.textContent = rgba;
 
+    console.log(rgba);
+
+    ctx.beginPath();
+    ctx.arc(x,y,1,0,2*Math.PI);
+    ctx.stroke();
 
   }
 
-  pick(event) {
 
-
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-
-
-
-
-
-    var x = event.layerX;
-    var y = event.layerY;
-    console.log(event,x,y)
-
-    // var pixel = ctx.getImageData(x, y, 1, 1);
-    // var data = pixel.data;
-    // var rgba = 'rgba(' + data[0] + ',' + data[1] +
-    //            ',' + data[2] + ',' + (data[3] / 255) + ')';
-    // color.style.background =  rgba;
-    // color.textContent = rgba;
-
-
-  }
 
   readToArray() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+    // const canvas = document.getElementById('canvas');
+    const ctx = this.refs.canvas.getContext('2d');
 
 
 
-    ctx.drawImage($('img')[0], 0,0)
-    ctx.getImageData(0,0,10,10)
+    // ctx.drawImage($('img')[0], 0,0)
+    ctx.getImageData(0,0,d,d)
 
 
 
 
-    const pixels = ctx.getImageData(0,0,10,10).data;
-    const w = ctx.getImageData(0,0,10,10).width;
-    const h = ctx.getImageData(0,0,10,10).height;
+    const pixels = ctx.getImageData(0,0,d,d).data;
+    const w = ctx.getImageData(0,0,d,d).width;
+    const h = ctx.getImageData(0,0,d,d).height;
 
     const arr = [];
     for (let i = 0; i < w * h; i++) {
@@ -82,7 +66,8 @@ export default class App extends React.Component {
 
       arr.push({ r, g, b, a, x});
     }
-    console.log(arr.map(e => e.x < 0.5 && 1 || 0))
+    // console.log(arr);
+    console.log(arr.map(e => e.a/255 > 0.5 && 1 || 0))
     return arr;
 
   }
@@ -90,9 +75,9 @@ export default class App extends React.Component {
   render() {
 
     return (<div>
-      <img src="Untitled.bmp" />
-      <h1 onClick={this.readToArray}>Hello!</h1>
-      <canvas id="canvas" width="10" height="10" style={{"border": "1px solid black"}} onMouseDown={this.paint} onMouseMove={this.pick}></canvas>
+      <img src="Untitled.bmp" onClick={this.readToArray.bind(this)} />
+      <h1>Hello!</h1>
+      <canvas id="canvas" ref="canvas" width={d} height={d} style={{"border": "1px solid black"}} onMouseDown={this.paint.bind(this)}></canvas>
     </div>);
   }
 }
